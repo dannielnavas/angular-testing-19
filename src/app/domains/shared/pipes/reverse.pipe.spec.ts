85fef9fb45613ecd34de404850c122b5c3ed68db
Danniel Navas <dannielnavas@gmail.com>
2025-04-10 07:44:29 -0500

    refactor: eliminar línea en blanco y mejorar pruebas para ReversePipe

--- src/app/domains/shared/pipes/reverse.pipe.spec.ts
+++ src/app/domains/shared/pipes/reverse.pipe.spec.ts
@@ -1,5 +1,4 @@
 import { SpectatorPipe, createPipeFactory } from '@ngneat/spectator/jest';
-
 import { ReversePipe } from './reverse.pipe';
 
 describe('ReversePipe', () => {
@@ -9,5 +8,25 @@ describe('ReversePipe', () => {
   it('should create an instance', () => {
     spectator = createPipe(`{{ 'Hello' | reverse }}`);
     expect(spectator.element).toHaveText('olleH');
+  });
+
+  it('debería invertir una cadena de texto simple', () => {
+    spectator = createPipe(`{{ 'Hello' | reverse }}`);
+    expect(spectator.element).toHaveText('olleH');
+  });
+
+  it('debería invertir una cadena con espacios', () => {
+    spectator = createPipe(`{{ 'angular testing' | reverse }}`);
+    expect(spectator.element).toHaveText('gnitset ralugna');
+  });
+
+  it('debería invertir una cadena con caracteres especiales', () => {
+    spectator = createPipe(`{{ 'a-b@c#123' | reverse }}`);
+    expect(spectator.element).toHaveText('321#c@b-a');
+  });
+
+  it('debería devolver una cadena vacía si se proporciona una cadena vacía', () => {
+    spectator = createPipe(`{{ '' | reverse }}`);
+    expect(spectator.element).toHaveText('');
   });
 });

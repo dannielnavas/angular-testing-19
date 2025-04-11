8adec7c0668d013ee172897e8da5f2304884bbd7
Danniel Navas <dannielnavas@gmail.com>
2025-04-11 07:47:22 -0500

    feat: agregar mapeo de módulo para el dominio de información en jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ export default {
     '@shared/(.*)$': '<rootDir>/src/app/domains/shared/$1',
     '@products/(.*)$': '<rootDir>/src/app/products/$1',
     '@env/(.*)$': '<rootDir>/src/environments/$1',
+    '@info/(.*)$': '<rootDir>/src/app/domains/info/$1',
   },
   collectCoverage: true,
   coverageReporters: ['html', 'text-summary'],

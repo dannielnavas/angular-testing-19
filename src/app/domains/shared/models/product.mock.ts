c3d0ad1e2a9d2b62970a7d3a5c91107a6e0d71b8
Danniel Navas <dannielnavas@gmail.com>
2025-04-14 07:43:22 -0500

    feat: actualizar generación de productos falsos para usar generateFakeCategory

--- src/app/domains/shared/models/product.mock.ts
+++ src/app/domains/shared/models/product.mock.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+import { generateFakeCategory } from './category.mock';
 import { Product } from './product.model';
 
 export const generateFakeProduct = (data?: Partial<Product>): Product => ({
@@ -8,12 +9,7 @@ export const generateFakeProduct = (data?: Partial<Product>): Product => ({
   price: parseFloat(faker.commerce.price()),
   images: [faker.image.url()],
   creationAt: faker.date.past().toISOString(),
-  category: {
-    id: faker.number.int(),
-    name: faker.commerce.department(),
-    image: faker.image.url(),
-    slug: faker.lorem.slug(),
-  },
+  category: generateFakeCategory(data?.category),
   slug: faker.lorem.slug(),
   ...data,
 });

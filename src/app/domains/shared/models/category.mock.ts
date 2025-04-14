61ca42587bbfc1bfb993aaa30000408e66dee0a0
Danniel Navas <dannielnavas@gmail.com>
2025-04-14 07:43:28 -0500

    feat: agregar función para generar categorías falsas

---
+++ src/app/domains/shared/models/category.mock.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+import { Category } from './category.model';
+
+export const generateFakeCategory = (data?: Partial<Category>): Category => ({
+  id: faker.number.int(),
+  name: faker.commerce.department(),
+  image: faker.image.url(),
+  slug: faker.lorem.slug(),
+  ...data,
+});
+1;

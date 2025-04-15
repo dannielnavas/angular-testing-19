3b47366bb66ebc66bb7ad651760f275095258ea4
Danniel Navas <dannielnavas@gmail.com>
2025-04-15 07:52:44 -0500

    feat: mejorar pruebas unitarias en ProductComponent al utilizar un producto simulado y agregar verificación del título con data-testid

--- src/app/domains/products/components/product/product.component.spec.ts
+++ src/app/domains/products/components/product/product.component.spec.ts
@@ -1,4 +1,8 @@
-import { createRoutingFactory, Spectator } from '@ngneat/spectator/jest';
+import {
+  byTestId,
+  createRoutingFactory,
+  Spectator,
+} from '@ngneat/spectator/jest';
 
 import { generateFakeProduct } from '@shared/models/product.mock';
 import { ProductComponent } from './product.component';
@@ -10,15 +14,29 @@ describe('ProductComponent', () => {
     component: ProductComponent,
   });
 
+  const mockProduct = generateFakeProduct();
+
   beforeEach(() => {
     spectator = createComponent({
       detectChanges: false,
     });
-    spectator.setInput('product', generateFakeProduct());
+    spectator.setInput('product', mockProduct);
   });
 
   it('should create', () => {
     spectator.detectChanges();
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('should display product title', () => {
+    spectator.detectChanges();
+    expect(spectator.query('h3')).toHaveText(mockProduct.title);
+  });
+
+  it('should display product title data-testid', () => {
+    spectator.detectChanges();
+    // const element = spectator.query('[data-testid="product-title"]');
+    const element = spectator.query(byTestId('product-title'));
+    expect(element).toHaveText(mockProduct.title);
   });
 });

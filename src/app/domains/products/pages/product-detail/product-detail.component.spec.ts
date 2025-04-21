b13b25ffe6911d29fa29fe3ee80812ee863c718c
Danniel Navas <dannielnavas@gmail.com>
2025-04-21 07:35:31 -0500

    feat: agregar pruebas unitarias para el componente ProductDetail y simular IntersectionObserver

--- src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
@@ -1,3 +1,4 @@
+import { DeferBlockBehavior } from '@angular/core/testing';
 import {
   byTestId,
   createRoutingFactory,
@@ -5,10 +6,31 @@ import {
   Spectator,
   SpyObject,
 } from '@ngneat/spectator/jest';
+import { RelatedComponent } from '@products/components/related/related.component';
 import { generateFakeProduct } from '@shared/models/product.mock';
 import { ProductService } from '@shared/services/product.service';
 import { of } from 'rxjs';
 import ProductDetailComponent from './product-detail.component';
+
+// Object.defineProperty(window, 'IntersectionObserver', {
+//   writable: true,
+//   configurable: true,
+//   value: jest.fn(() => ({
+//     observe: jest.fn(),
+//     unobserve: jest.fn(),
+//     disconnect: jest.fn(),
+//   })),
+// });
+
+window.IntersectionObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+  takeRecords: jest.fn(),
+  root: null,
+  rootMargin: '',
+  thresholds: [],
+}));
 
 describe('ProductDetail', () => {
   let spectator: Spectator<ProductDetailComponent>;
@@ -18,6 +40,7 @@ describe('ProductDetail', () => {
 
   const createComponent = createRoutingFactory({
     component: ProductDetailComponent,
+    deferBlockBehavior: DeferBlockBehavior.Manual,
     providers: [
       mockProvider(ProductService, {
         getOneBySlug: jest.fn().mockReturnValue(of(mockProduct)),
@@ -29,6 +52,7 @@ describe('ProductDetail', () => {
       // props: {
       //   slug: mockProduct.slug,
       // },
+
       detectChanges: false,
     });
     spectator.setInput('slug', mockProduct.slug);
@@ -40,6 +64,14 @@ describe('ProductDetail', () => {
     expect(spectator.component).toBeTruthy();
   });
 
+  it('Should call the productService with the correct slug', () => {
+    // Arrange
+    // Act
+    spectator.detectChanges();
+    // Assert
+    expect(productService.getOneBySlug).toHaveBeenCalledWith(mockProduct.slug);
+  });
+
   it('Should display the product cover', () => {
     // Arrange
     // productService.getOneBySlug.mockReturnValue(of(mockProduct)); ya viene en el mockProvider
@@ -49,5 +81,12 @@ describe('ProductDetail', () => {
     // Assert
     expect(img).toBeTruthy();
     expect(img?.src).toBe(mockProduct.images[0]);
+  });
+
+  it('Should load related products', async () => {
+    spectator.detectChanges();
+    await spectator.deferBlock().renderComplete();
+    const related = spectator.query(RelatedComponent);
+    expect(related).toBeTruthy();
   });
 });

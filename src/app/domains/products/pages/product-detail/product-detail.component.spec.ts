b3561b11e8ca60e49fe67120f072ce9054122a9b
Danniel Navas <dannielnavas@gmail.com>
2025-04-21 07:23:44 -0500

    feat: mejorar pruebas unitarias del componente ProductDetail con mock del servicio

--- src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
@@ -1,30 +1,53 @@
 import {
+  byTestId,
   createRoutingFactory,
   mockProvider,
   Spectator,
+  SpyObject,
 } from '@ngneat/spectator/jest';
 import { generateFakeProduct } from '@shared/models/product.mock';
 import { ProductService } from '@shared/services/product.service';
+import { of } from 'rxjs';
 import ProductDetailComponent from './product-detail.component';
 
 describe('ProductDetail', () => {
   let spectator: Spectator<ProductDetailComponent>;
+  let productService: SpyObject<ProductService>;
 
   const mockProduct = generateFakeProduct();
 
   const createComponent = createRoutingFactory({
     component: ProductDetailComponent,
-    providers: [mockProvider(ProductService)],
+    providers: [
+      mockProvider(ProductService, {
+        getOneBySlug: jest.fn().mockReturnValue(of(mockProduct)),
+      }),
+    ],
   });
   beforeEach(() => {
     spectator = createComponent({
-      props: {
-        slug: mockProduct.slug,
-      },
+      // props: {
+      //   slug: mockProduct.slug,
+      // },
+      detectChanges: false,
     });
+    spectator.setInput('slug', mockProduct.slug);
+    productService = spectator.inject(ProductService);
   });
 
   it('should create', () => {
+    spectator.detectChanges();
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('Should display the product cover', () => {
+    // Arrange
+    // productService.getOneBySlug.mockReturnValue(of(mockProduct)); ya viene en el mockProvider
+    // Act
+    spectator.detectChanges();
+    const img = spectator.query<HTMLImageElement>(byTestId('cover'));
+    // Assert
+    expect(img).toBeTruthy();
+    expect(img?.src).toBe(mockProduct.images[0]);
   });
 });
